1472206ffe1b12ef5f6f4a36a28e2f307378eb8a
Demaris Ashmore <demiashmore@yahoo.com>
2023-03-05 13:06:23 -0700

    commit tree ecacc4953e8ec17d3af5fbce32ac1bef2e239e17

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,5 +10,18 @@ class LinkedList
 
   def reverse!
     # your code here
+    return if head.nil?
+    curr = head
+    tail_node = head
+    prev_node = nil
+    while curr != nil
+      next_node = curr.next_node
+      curr.next_node = prev_node
+      curr.prev_node = next_node
+      prev_node = curr
+      curr = next_node
+    end
+    @head = prev_node
+    @tail = tail_node
   end
 end
